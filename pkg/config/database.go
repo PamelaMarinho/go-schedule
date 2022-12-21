6d6d115032ca8fd67330ee865dcbbcf33340873c
Pamela Silva <silvamrt6@gmail.com>
2022-12-21 13:24:58 -0300

    config database

--- pkg/config/database.go
+++ pkg/config/database.go
@@ -1,1 +1,19 @@
 package config
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+func Connection() {
+	database, err := gorm.Open("mysql", "root:124086@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = database
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
